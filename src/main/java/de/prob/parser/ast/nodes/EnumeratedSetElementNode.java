412ea03d0f364e50bc0e9e40b6e97324b5121216
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2019-06-06 20:29:59 +0200

    Revert deleting EnumeratedSetElementNode

---
+++ src/main/java/de/prob/parser/ast/nodes/EnumeratedSetElementNode.java
@@ -0,0 +1,34 @@
+package de.prob.parser.ast.nodes;
+
+import de.prob.parser.ast.SourceCodePosition;
+import de.prob.parser.ast.nodes.expression.ExprNode;
+
+public class EnumeratedSetElementNode extends ExprNode {
+
+    final EnumeratedSetDeclarationNode enumeratedSetDeclarationNode;
+    final DeclarationNode declarationNode;
+    final String elementName;
+
+    public EnumeratedSetElementNode(SourceCodePosition sourceCodePosition,
+                                    EnumeratedSetDeclarationNode enumeratedSetDeclarationNode, String elementName,
+                                    DeclarationNode declarationNode) {
+        super(sourceCodePosition);
+        this.enumeratedSetDeclarationNode = enumeratedSetDeclarationNode;
+        this.elementName = elementName;
+        this.declarationNode = declarationNode;
+        this.enumeratedSetDeclarationNode.setParent(this);
+        this.declarationNode.setParent(this);
+    }
+
+    public String getName() {
+        return this.elementName;
+    }
+
+    public EnumeratedSetDeclarationNode getEnumeratedSetDeclarationNode() {
+        return this.enumeratedSetDeclarationNode;
+    }
+
+    public DeclarationNode getDeclarationNode() {
+        return declarationNode;
+    }
+}
