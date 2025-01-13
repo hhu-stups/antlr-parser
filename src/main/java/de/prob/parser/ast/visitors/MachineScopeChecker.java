9bb5515ebd1d9b7bd6748bb7873e2220f96b133c
Fabian Vu <fabian.vu@hhu.de>
2025-01-13 10:39:25 +0100

    Add RDIV

--- src/main/java/de/prob/parser/ast/visitors/MachineScopeChecker.java
+++ src/main/java/de/prob/parser/ast/visitors/MachineScopeChecker.java
@@ -65,6 +65,7 @@ public class MachineScopeChecker {
 		externalFunctionsAndVariables.put("RpcArray", new DeclarationNode(machineNode.getSourceCodePosition(), "RpcArray", DeclarationNode.Kind.CONSTANT, machineNode));
 		externalFunctionsAndVariables.put("RpcInteger", new DeclarationNode(machineNode.getSourceCodePosition(), "RpcInteger", DeclarationNode.Kind.CONSTANT, machineNode));
 		externalFunctionsAndVariables.put("RpcFloat", new DeclarationNode(machineNode.getSourceCodePosition(), "RpcFloat", DeclarationNode.Kind.CONSTANT, machineNode));
+		externalFunctionsAndVariables.put("RDIV", new DeclarationNode(machineNode.getSourceCodePosition(), "RDIV", DeclarationNode.Kind.CONSTANT, machineNode));
 		externalFunctionsAndVariables.put("floor", new DeclarationNode(machineNode.getSourceCodePosition(), "floor", DeclarationNode.Kind.CONSTANT, machineNode));
 		externalFunctionsAndVariables.put("ZMQ_RPC_INIT", new DeclarationNode(machineNode.getSourceCodePosition(), "ZMQ_RPC_INIT", DeclarationNode.Kind.CONSTANT, machineNode));
 		try {
